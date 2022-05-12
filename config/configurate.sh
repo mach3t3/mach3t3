5a6beea269647d9f9a311be55e1c2cb50b0ffe29
mach3t3 <mach3t333@gmail.com>
2022-05-12 22:08:03 +0200

    Added correct file path's

--- config/configurate.sh
+++ config/configurate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Downloading configuration files"
-curl https://raw.githubusercontent.com/mach3t3/mach3t3/develop/.editorconfig --output .editorconfig
-curl https://raw.githubusercontent.com/mach3t3/mach3t3/develop/.prettierrc --output .prettierrc
+curl https://raw.githubusercontent.com/mach3t3/mach3t3/main/config/.editorconfig --output .editorconfig
+curl https://raw.githubusercontent.com/mach3t3/mach3t3/main/config/.prettierrc --output .prettierrc
